edba1b8184e016cf98205bd26b0746565a118d60
Trikaal212 <anikets993@gmail.com>
2025-11-02 22:58:08 +0530

    changes in api

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -40,7 +40,9 @@ productRouter.get("/my-products", authMiddleware, getMyProducts);
 productRouter.get("/:id", getProductById);
 
 // ✅ Now uploads go to Cloudinary instead of local folder
-productRouter.post("/", authMiddleware, upload.array("images", 5), (req, res, next) => {
+productRouter.post
+console.log("start")
+("/", authMiddleware, upload.array("images", 5), (req, res, next) => {
   console.log("📸 Uploaded files:", req.files);
   console.log("📦 Request body:", req.body);
   next();
