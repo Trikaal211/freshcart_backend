e1938b725a048d2cd0b5801256b2f31ef419600a
Trikaal212 <anikets993@gmail.com>
2025-11-03 11:57:15 +0530

    changes in api

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -10,9 +10,9 @@ console.log("Cloudinary Config Check:", {
 });         
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
