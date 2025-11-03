c2661827b86fd52db331138488f816f9de71c4a3
Trikaal212 <anikets993@gmail.com>
2025-11-03 15:10:08 +0530

    changes in api

--- config/multer.js
+++ config/multer.js
@@ -1,16 +1,23 @@
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
-import cloudinary from "./cloudinary.js"; 
+import cloudinary from "./cloudinary.js";
+
+console.log("🟢 Initializing multer...");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
-    folder: "products",
-    allowed_formats: ["jpg", "jpeg", "png"],
-    transformation: [{ width: 800, height: 800, crop: "limit" }],
+  params: async (req, file) => {
+    console.log("📸 File details inside multer:", file.originalname);
+    return {
+      folder: "products",
+      allowed_formats: ["jpg", "jpeg", "png"],
+      transformation: [{ width: 800, height: 800, crop: "limit" }],
+    };
   },
 });
-console.log("heyheyeyey")
+
 const upload = multer({ storage });
+
+console.log("✅ Multer ready");
 
 export { upload };
