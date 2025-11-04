85d754959e75d9241ac3e6823d00d4d616805534
Trikaal212 <anikets993@gmail.com>
2025-11-04 20:30:59 +0530

    changes in api

--- src/routers/order.routes.js
+++ src/routers/order.routes.js
@@ -1,18 +1,11 @@
-// In your order router
 import express from "express";
-import {
-  createOrder,
-  getUserOrders,
-  getAllOrders,
- updateProductOrderStatus // Make sure this is imported
-} from "../controllers/order.controller.js";
-import { authMiddleware } from "../../middlewares/user.middleware.js";
+import { createOrder, getMyOrders, updateOrderStatus } from "../controllers/order.controller.js";
+import { authMiddleware} from "../../middlewares/user.middleware.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/", authMiddleware, createOrder);
-orderRouter.get("/my-orders", authMiddleware, getUserOrders);
-orderRouter.get("/", authMiddleware, getAllOrders);
-orderRouter.patch("/:id/status", authMiddleware,updateProductOrderStatus); // Fixed route
+orderRouter.get("/my-orders", authMiddleware, getMyOrders);
+orderRouter.patch("/:orderId/status", authMiddleware, updateOrderStatus);
 
 export default orderRouter;
